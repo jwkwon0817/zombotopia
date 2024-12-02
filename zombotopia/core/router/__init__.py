4fa064147b776ff39ddbd28fdec96c0f326ff79b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-12-02 11:57:39 +0900

    feat: continuous deployment

--- zombotopia/core/router/__init__.py
+++ zombotopia/core/router/__init__.py
@@ -4,5 +4,8 @@ from .ranking import router as ranking_router
 
 router = APIRouter()
 
+@router.get('/test')
+def test():
+    return {"message": "Hello World"}
 
 router.include_router(ranking_router, prefix="/ranking", tags=["ranking"])
